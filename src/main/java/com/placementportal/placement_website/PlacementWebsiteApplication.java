264c8356aec44c478cdae202df833e633560e66d
RandomguyRC <rahul.student.cse23@itbhu.ac.in>
2025-09-15 22:29:31 +0530

    Initial commit: Spring Boot placement website

---
+++ src/main/java/com/placementportal/placement_website/PlacementWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.placementportal.placement_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementWebsiteApplication.class, args);
+	}
+
+}
